0f60c387c2cf69626edcc943d3c4359cc1e3b07b
Jayant Prakash <jn6878@att.com>
2025-11-18 23:37:51 -0500

    create prompt template

---
+++ BuildingWorkflowWithLanggraph/prompt_templates.py
@@ -0,0 +1,97 @@
+#Prompt templates
+import os
+import sys
+import importlib.util
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import SystemMessage, HumanMessage
+from langchain_community.llms import FakeListLLM
+
+# Absolute path to config.py (adjust as needed)
+config_path = "/Users/jn6878/Documents/config.py"
+
+spec = importlib.util.spec_from_file_location("config", config_path)
+config = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(config)
+
+# Now you can use config.set_environment()
+config.set_environment()
+
+
+base_url = os.environ.get("OPEN_AI_LITE_LLM_BASE_URL")
+api_key = os.environ.get("OPENAI_API_KEY")
+llm = ChatOpenAI(model="gpt-4.1", api_key=api_key,base_url = base_url)
+
+job_description = """
+SPS-Software Engineer (m/w/d) im Maschinenbau
+Glaston Germany GmbH
+Neuhausen-Hamberg
+Feste Anstellung
+Homeoffice möglich, Vollzeit
+Erschienen: vor 1 Tag
+Glaston Germany GmbH logo
+SPS-Software Engineer (m/w/d) im Maschinenbau
+Glaston Germany GmbH
+slide number 1slide number 2slide number 3
+Glaston ist eine internationale Marke mit weltweit führenden Unternehmen, die für zukunftsweisende Maschinen, Anlagen, Systeme und Dienstleistungen in der Bearbeitung von Architektur-, Fahrzeug- und Displayglas steht.
+
+Mit unserer über 50-jährigen Erfahrung am Standort Glaston Germany GmbH in Neuhausen bei Pforzheim verbessern und sichern wir nachhaltig die Produktivität unserer Kunden bei der Fertigung von Architekturglas. Diesen Erfolg verdanken wir unseren motivierten und engagierten Mitarbeitenden und wurden so zu einem der führenden Anbieter von automatisierten und kundenspezifischen Anlagen.
+
+Der Umgang mit Software liegt dir im Blut und du möchtest bei einem Hidden Champion durchstarten?
+Dein Faible für Softwarelösungen und dein Herz für unterschiedliche Technologien sind ideale Voraussetzungen, um Maschinen wieder zu alter Stärke zu verhelfen?
+Du hast einen ausgeprägten Servicegedanken und Spaß an der Arbeit mit Kunden?
+
+Dann komm zu Glaston! Wir suchen ab sofort für unseren Bereich Service Upgrades Verstärkung!
+
+SPS-SOFTWARE ENGINEER (M/W/D) IM MASCHINENBAU
+
+Als SPS-Software Engineer (m/w/d) im Maschinenbau sind deine Aufgabengebiete:
+Ausarbeitung und Weiterentwicklung von Kundenaufträgen und Upgrade-Konzepten
+Selbstständige und termingerechte Bearbeitung von Kundenprojekten und Bereitstellung der notwendigen Dokumente
+Unterstützung des Inbetriebnahme- und Servicepersonals im Haus und beim Kunden vor Ort
+Diese Anforderungen musst du mitbringen:
+Qualifizierte technische Ausbildung: Techniker, Studium oder vergleichbare Qualifikation
+Mehrjährige Berufserfahrung im Serviceumfeld, idealerweise im Maschinen- und Anlagenbau
+Umfangreiche Kenntnisse in verschiedenen SPS-Programmiersprachen (z.B. S7Classic, TIA, Simotion)
+Bei uns profitierst du von folgenden Benefits:
+Exzellente Rahmenbedingungen (z.B. attraktives Gehaltsmodell, flexible Arbeitszeiten mit Gleitzeit und Homeoffice-Möglichkeiten)
+Attraktives Arbeitsumfeld in idyllisch-ländlicher Lage
+Umfangreiche Mobilitätsförderung (z.B. Ladestation für Elektroautos)
+Wellbeing am Arbeitsplatz
+"""
+
+prompt_template = (
+    "Given a job description, decide whether it suites a junior Java developer."
+    "\nJOB DESCRIPTION:\n{job_description}\n"
+)
+result = llm.invoke(prompt_template.format(job_description=job_description))
+print(result.content)
+
+from langchain_core.prompts import PromptTemplate
+
+lc_prompt_template = PromptTemplate.from_template(prompt_template)
+print(lc_prompt_template.invoke({"job_description": "fake_jd"}))
+
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate, HumanMessagePromptTemplate
+from langchain_core.messages import SystemMessage, HumanMessage
+from langchain_core.prompts import SystemMessagePromptTemplate
+
+lc_prompt_template = PromptTemplate.from_template(prompt_template)
+chain = lc_prompt_template | llm | StrOutputParser()
+print(chain.invoke({"job_description": job_description}))
+
+chat_prompt_template = ChatPromptTemplate.from_messages(
+    [("system", "You are a helpful assistant."),
+     ("human", prompt_template)])
+chain = chat_prompt_template | llm | StrOutputParser()
+print(chain.invoke({"job_description": job_description}))
+
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+
+chat_prompt_template = ChatPromptTemplate.from_messages(
+    [("system", "You are a helpful assistant."),
+     ("placeholder", "{history}"),
+     # same as MessagesPlaceholder("history"),
+     ("human", prompt_template)])
+
+print(chat_prompt_template.invoke("fake"))
